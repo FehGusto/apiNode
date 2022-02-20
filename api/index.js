05a5c1b079790d2df33e27f5f764902362240eda
FehGusto <felipe.santos.ofic@gmail.com>
2022-02-20 20:18:24 -0300

    Começando uma API REST Aulo 05

--- api/index.js
+++ api/index.js
@@ -5,9 +5,27 @@ const config = require('config')
 const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
-
+const formatosAceitos = require('./Serializador').formatosAceitos
 
 app.use(bodyParser.json())
+
+app.use((requisicao, resultado, proximo) => {
+    let formatResquis = requisicao.header('Accept')
+
+    if (formatResquis === '*/*') {
+        formatResquis = 'application/json'
+    }
+
+    if (formatosAceitos.indexOf(formatResquis) === -1) {
+        resultado.status(406)
+        resultado.end()
+        return
+    }
+
+    resultado.setHeader('Content-Type', formatResquis)
+    proximo()
+})
+
 
 const roteador = require('./rotas/fornecedores')
 app.use('/api/fornecedores', roteador)
